569c94d2edef21efcde98fadb370430fda1c36a1
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-01 16:20:09 +0500

    Добавьте файлы проекта.

---
+++ ForReletion/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+namespace SignalR
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HubConnection? connection = new HubConnectionBuilder().WithUrl("https://localhost:7052/messenger").Build();
+
+            connection.On<string, string>("Recieve", (name, message) =>
+            {
+                Console.WriteLine($"{name}: {message}");
+            });
+
+            connection.On<string>("Welcome", message =>
+            {
+                Console.WriteLine(message);
+            });
+
+            connection.On<string>("Joined", message =>
+            {
+                Console.WriteLine(message);
+            });
+
+            await connection.StartAsync();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            await connection.SendAsync("Start", name);
+
+            while (true)
+            {
+                string? console = Console.ReadLine();
+
+                await connection.SendAsync("SendMessage", console);
+            }
+        }
+    }
+}
